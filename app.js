8d48c1d9175d1ab38f6f6277e5e85f12d482a7ff
Daeyeol Ryu <dy@yoobato.com>
2017-10-09 04:40:41 +0900

    Change port to 8051 (avoid conflict with rahasia-api)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', require('./routes/sms'));
 
-var port = process.env.PORT || 8050;
+var port = process.env.PORT || 8051;
 
 console.log('Cafe24SMS 서버 시작 (' + port + ')');
 app.listen(port);
