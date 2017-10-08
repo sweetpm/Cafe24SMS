9712e01a9ad420a4082719761fea146bee298c29
Daeyeol Ryu <dy@yoobato.com>
2017-10-09 04:55:13 +0900

    Add root GET to inform

--- routes/sms.js
+++ routes/sms.js
@@ -4,6 +4,10 @@ var express = require('express');
 var router = express.Router();
 var sms = require('../sms/sms');
 
+router.get('/', (req, res) => {
+  return res.send('Use POST /send');
+});
+
 router.post('/send', (req, res) => {
   var receiverPhone = req.body.receiverPhone;
   var message = req.body.message;
